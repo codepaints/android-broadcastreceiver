b0543c0618b79e438427616368f052c796a512f5
Q2A Market <dev@q2amarket.com>
2019-02-19 17:23:43 +0400

    Initial commits

---
+++ app/src/main/java/me/jatinsoni/broadcastreceiver/MyBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package me.jatinsoni.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Broadcast Receiver Triggered!", Toast.LENGTH_SHORT).show();
+        Log.d("BroadcastReceiver", "Broadcast Receiver Triggered!");
+    }
+}
